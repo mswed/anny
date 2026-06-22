f73d1aa50d009817afe5d17c58d7c0aed70227ba
Moshe Swed <mswed@beapot.com>
2026-06-22 16:11:16 -0400

    FreehandStroke draws with LINE_STRIP

--- streamdeck/anny-start.sh
+++ streamdeck/anny-start.sh
@@ -7,14 +7,15 @@ PROJECT="/home/mswed/Documents/coding/anny"
 
 # Only create if session doesn't already exist
 if ! tmux has-session -t "$SESSION" 2>/dev/null; then
-    tmux new-session -d -s "$SESSION" -n nvim -c "$PROJECT"
-    tmux send-keys -t "$SESSION:nvim" "nvim ." Enter
+  tmux new-session -d -s "$SESSION" -n nvim -c "$PROJECT"
+  tmux send-keys -t "$SESSION:nvim" "source .venv/bin/activate" Enter
+  tmux send-keys -t "$SESSION:nvim" "nvim ." Enter
 
-    tmux new-window -t "$SESSION" -n git -c "$PROJECT"
-    tmux send-keys -t "$SESSION:git" "git status" Enter
+  tmux new-window -t "$SESSION" -n git -c "$PROJECT"
+  tmux send-keys -t "$SESSION:git" "git status" Enter
 
-    tmux new-window -t "$SESSION" -n rv -c "$HOME"
-    tmux send-keys -t "$SESSION:rv" "~/openrv.sh" Enter
+  tmux new-window -t "$SESSION" -n rv -c "$HOME"
+  tmux send-keys -t "$SESSION:rv" "~/openrv.sh" Enter
 fi
 
 # Attach in guake
